fb6e72e7c31a635300262a2f730e3b8f9644810b
Visswateza Rajavarapu <visswatezarajavarapu@gmail.com>
2022-12-03 05:51:02 +0000

    mergerd route using route

--- app.js
+++ app.js
@@ -4,8 +4,21 @@ const PORT = 3000;
 
 app.get('/', (req, res)=>{
     res.send('<h1>Hello World!</h1>');
-})
+});
+
+app.route("/users")
+    .get("/", (req, res)=>{
+        res.send("All users");
+    })
+    .post("/", (req, res)=>{
+        res.send("User created");
+    });
+
+
+app.get("/*", (req, res)=>{
+    res.send("Page not found");
+});
 
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`);
-})
+});
